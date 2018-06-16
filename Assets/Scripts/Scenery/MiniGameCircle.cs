653ad0c0edcfb5504b4ab7f9295ed4cc0728a73c
ingridsl <inus2agome@gmail.com>
2018-06-15 21:51:53 -0300

    começando o mini game (rascunho não funcional)

---
+++ Assets/Scripts/Scenery/MiniGameCircle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using CrowShadowManager;
+/// <summary>
+/// https://www.youtube.com/watch?v=BGe5HDsyhkY
+/// </summary>
+namespace CrowShadowScenery
+{
+    public class MiniGameCircle : MonoBehaviour
+    {
+        private float RotateSpeed = 5f;
+        private float Radius = 0.1f;
+
+        private Vector2 _centre;
+        private float _angle;
+
+        private void Start()
+        {
+            _centre = transform.position;
+        }
+
+        private void Update()
+        {
+
+            _angle += RotateSpeed * Time.deltaTime;
+
+            var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
+            transform.position = _centre + offset;
+        }
+
+    }
+}
