8223c4147b8467b9af80ff5fdb724263c75c8fd7
ingridsl <inus2agome@gmail.com>
2018-07-12 21:01:25 -0300

    Flashback missão 5

--- Assets/Scripts/Manager/SideQuests/SideQuest1.cs
+++ Assets/Scripts/Manager/SideQuests/SideQuest1.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using CrowShadowManager;
 using CrowShadowScenery;
+using CrowShadowNPCs;
 
 public class SideQuest1 : SideQuest
 {
-    SpiritManager spiritManager;
+    GameObject person1, person2;
+       SpiritManager spiritManager;
 
     public override void InitSideQuest()
     {
@@ -47,12 +49,32 @@ public class SideQuest1 : SideQuest
             counterTimeEscape = timeEscape;
             SetTimeToEscape();
         }
-
+        ////if (success)
+        //{
+//
+           //EndSideQuest();
+        //}
         SpinCamera(5f);
     }
 
     public override void ShowFlashback()
     {
+        GameManager.instance.rpgTalk.NewTalk("M5Side1FlashbackStart", "M5Side1FlashbackEnd", GameManager.instance.rpgTalk.txtToParse);
+        // Pessoa 1
+        person1 = GameManager.instance.AddObject("NPCs/personShadow", "", new Vector3(2f, 0.5f, -0.5f), new Vector3(0.3f, 0.3f, 1));
+        person1.GetComponent<Patroller>().isPatroller = true;
+        Vector3 auxP1 = new Vector3(2f, -2f, -0.5f);
+        Vector3 auxP2 = new Vector3(-2f, -2f, -0.5f);
+        Vector3 auxP3 = new Vector3(-2f, 1f, -0.5f);
+        Vector3 auxP4 = new Vector3(-2f, 0f, -0.5f);
+        Vector3 auxP5 = new Vector3(-1f, 0f, -0.5f);
+        Vector3 auxP6 = new Vector3(-2f, 0f, -0.5f);
+        Vector3 auxP7 = new Vector3(-2f, -2f, -0.5f);
+        Vector3 auxP8 = new Vector3(2f, -2f, -0.5f);
+        Vector3[] p1Pos = { auxP1, auxP2, auxP3, auxP4 , auxP5, auxP6, auxP7, auxP8};
+        person1.GetComponent<Patroller>().targets = p1Pos;
+        person1.GetComponent<Patroller>().speed = 0.9f;
+        person1.GetComponent<Patroller>().stopEndPath = true;
         EndFlashback();
     }
 
